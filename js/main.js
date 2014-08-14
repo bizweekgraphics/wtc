87b6bb08faeecc51af19b7719652222117455660
dyep49 <dyep49@gmail.com>
2014-08-14 17:31:08 -0400

    first draft

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 
+  $('li').each(function(index, item) {
+    $(item).mouseenter(function() {
+      $('#wtc-img').attr('src', "img/wtc_" + (index + 1) + ".png")
+      $(item).css('color', 'blue')
+      $(item).css('font-weight', 'bold')
+    })
 
+    $(item).mouseleave(function() {
+      $('#wtc-img').attr('src', "img/wtc_0.png")
+      $(item).css('color', 'black')
+      $(item).css('font-weight', 'normal')
+    })
+  })
 
   
 })
